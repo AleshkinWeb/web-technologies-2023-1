6787cd017d7835cb53bc91072d9bf9edaa6391c7
AxlifreewayWeb <ackackovich.akaky@yandex.ru>
2024-05-26 16:10:00 +0500

    Сделал 18!

--- index.php
+++ index.php
@@ -2,116 +2,101 @@
 
 //Задание 1
 
-$a = 9;
-$b = 10;
-
-if ($a >= 0 and $b >= 0) {
-    echo($a - $b);
-} else if ($a < 0 and $b < 0) {
-    echo($a * $b);
-}
-else{
-    echo($a + $b);
+function printWhile(){
+    $iterator = 0;
+    $max = 10;
+    do{
+        if ($iterator == 0) {
+            echo $iterator . " - это ноль.\n";
+        }
+        else if($iterator % 2 == 0){
+            echo $iterator . " - это четное число.\n";
+        } else{
+            echo $iterator . " - это нечетное число.\n";
+        }
+        $iterator++;
+    }while($iterator <= $max);
+    
 }
 
 //Задание 2
-echo("\n");
-$a = 7;
 
-switch ($a) {
-    case 0:
-        echo(" 0");
-    case 1:
-        echo(" 1");
+$array = array("Московская область" => array("Москва", "Зеленоград", "Клин"),
+    "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
+    "Рязанская область" => array("Рязань", "Скопин", "Сасово"));
 
-    case 2:
-        echo(" 2");
-    case 3:
-        echo(" 3");
-    case 4:
-        echo(" 4");
-    case 5:
-        echo (" 5");
-    case 6:
-        echo(" 6");
-    case 7:
-        echo(" 7");
-    case 8:
-        echo(" 8");
-    case 9:
-        echo(" 9");
-    case 10:
-        echo(" 10");
-    case 11:
-        echo(" 11");
-    case 12:
-        echo(" 12");
-    case 13:
-        echo(" 13");
-    case 14:
-        echo(" 14");
-    case 15:
-        echo(" 15");
-        break;
-    default:
-        echo("Выходит за рамки массива!");
-}        
+foreach ($array as $city => $cities) {
+    echo ($city .":\n");
+    foreach ($cities as $town){
+        echo ($town .", ");
+    }
+    echo ("\n");
+}
+
 
 //Задание 3
 
-function summation($a, $b){
-    return $a + $b;
-}
 
-function subtraction($a, $b){
-    return $a - $b;
-}
+function transmorph($text) {
+    
+    $alphabet = array(
+        'а' => 'a',
+        'б' => 'b', 
+        'в' => 'v', 
+        'г' => 'g', 
+        'д' => 'd',
+        'е' => 'e', 
+        'ё' => 'yo', 
+        'ж' => 'zh', 
+        'з' => 'z', 
+        'и' => 'i',
+        'й' => 'y', 
+        'к' => 'k', 
+        'л' => 'l', 
+        'м' => 'm', 
+        'н' => 'n',
+        'о' => 'o', 
+        'п' => 'p', 
+        'р' => 'r', 
+        'с' => 's', 
+        'т' => 't',
+        'у' => 'u', 
+        'ф' => 'f', 
+        'х' => 'h', 
+        'ц' => 'ts', 
+        'ч' => 'ch',
+        'ш' => 'sh', 
+        'щ' => 'sch', 
+        'ъ' => '', 
+        'ы' => 'y', 
+        'ь' => '',
+        'э' => 'e', 
+        'ю' => 'yu', 
+        'я' => 'ya',
+    );
 
-function multiplication($a, $b){
-    return $a * $b;
-}
+    $result = '';
+    $text = mb_strtolower($text, 'UTF-8'); 
 
-function division($a, $b){
-    if($b != 0) {
-        return $a / $b;
-    }  
-}
-
-//Задание 4
-
-function mathOperation($arg1, $arg2, $operation){
-    switch($operation){
-        case "Умножение":
-            return multiplication($arg1, $arg2);
-            break;
-        case "Сложение":
-            return summation($arg1, $arg2);
-            break;
-        case "Вычетание":
-            return subtraction($arg1, $arg2);
-            break;
-        case "Деление":
-            return division($arg1, $arg2);
-            break;
-        default:
-            echo("Заданная функция ещё не реализована!");
-            break;
+    
+    for ($i = 0; $i < mb_strlen($text, 'UTF-8'); $i++) {
+        $char = mb_substr($text, $i, 1, 'UTF-8');
+        
+        if (array_key_exists($char, $alphabet)) {
+            $result .= $alphabet[$char];
+        } else {
+            $result .= $char; 
+        }
     }
-}
-
-//Задание 6
-
-function power($val, $pow){
-    if ($pow == 0) {
-        return 1;
-    }else if ($pow > 0) {
-        return $val * power($val, $pow - 1);
-    }else if ($pow < 0) {
-        return power($val, $pow + 1) / $val;
-    }
+    return $result;
 }
 
 //Задание 5
+
+
+
+//Задание 6
+
 
 ?>
 
