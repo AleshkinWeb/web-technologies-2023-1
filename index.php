1c4619c3abce65a778e2a59ef3ed6e64f22b6b34
AxlifreewayWeb <ackackovich.akaky@yandex.ru>
2024-06-01 14:53:39 +0500

    Новые пункты для 18

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 //Задание 1
 
 function printWhile(){
@@ -76,11 +75,11 @@ function transmorph($text) {
     );
 
     $result = '';
-    $text = mb_strtolower($text, 'UTF-8'); 
+    $text = mb_strtolower($text); 
 
     
-    for ($i = 0; $i < mb_strlen($text, 'UTF-8'); $i++) {
-        $char = mb_substr($text, $i, 1, 'UTF-8');
+    for ($i = 0; $i < mb_strlen($text); $i++) {
+        $char = mb_substr($text, $i, 1);
         
         if (array_key_exists($char, $alphabet)) {
             $result .= $alphabet[$char];
@@ -91,12 +90,58 @@ function transmorph($text) {
     return $result;
 }
 
+echo ("\n");
+
 //Задание 5
 
+echo ("\n");
 
+$menu =  [
+    ['name' => 'Меню', 'children' => 
+        [
+            [
+                'name' => 'Верхний уровень', 'children' => 
+                [
+                    [
+                        'name' => 'Средний уровень', 'children' => 
+                        [
+                            [
+                                'name' => 'Нижний уровень'
+                            ],
+                        ]
+                    ]
+                ]
+            ]
+        ],
+    ],
+];
+
+function createMenu($menu) {
+    echo ("<ul>");
+    foreach ($menu as $value) {
+        echo ("<li>");
+        echo  ("{$value['name']}");
+        if (isset($value['children'])) {
+            createMenu($value['children']);
+        }
+        echo ("</li>");
+    }
+    echo ("</ul>");
+}
+createMenu($menu);
 
 //Задание 6
-
+echo ("\n");
+foreach ($array as $city => $cities) {
+    echo ($city .":\n");
+    foreach ($cities as $town){
+        $str_town = mb_strtolower($town);
+        if (mb_substr($str_town, 0, 1) == 'к') {
+            echo ($str_town .", ");
+        }
+    }
+    echo ("\n");
+}
 
 ?>
 
